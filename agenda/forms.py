70ec6f0321b34ddb28a09358095e88b55d181666
Osvaldo Santana <osantana@triveos.com>
2010-04-26 17:08:36 -0300

    Editar objeto com ModelForm

--- agenda/forms.py
+++ agenda/forms.py
@@ -1,11 +1,14 @@
 # -*- encoding: utf-8 -*-
 
 from django import forms
+from agenda.models import ItemAgenda
 
-class FormItemAgenda(forms.Form):
-    titulo = forms.CharField(max_length=100)
+class FormItemAgenda(forms.ModelForm):
     data = forms.DateField(
                     widget=forms.DateInput(format='%d/%m/%Y'),
                     input_formats=['%d/%m/%Y', '%d/%m/%y'])
-    hora = forms.TimeField()
-    descricao = forms.CharField()
+
+    class Meta:
+        model = ItemAgenda
+        fields = ('titulo', 'data', 'hora', 'descricao')
+
