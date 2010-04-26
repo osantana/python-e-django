9d6096f28b0cd2fc965f48d3ce11417b8a6a3031
Osvaldo Santana <osantana@triveos.com>
2010-04-26 18:16:30 -0300

    Admin melhorado

--- agenda/admin.py
+++ agenda/admin.py
@@ -3,4 +3,18 @@
 from gerenciador.agenda.models import ItemAgenda
 from django.contrib import admin
 
-admin.site.register(ItemAgenda)
+class ItemAgendaAdmin(admin.ModelAdmin):
+    fields = ('data', 'hora', 'titulo', 'descricao')
+    list_display = ('data', 'hora', 'titulo')
+    list_display_links = ('data', 'hora', 'titulo')
+
+    def queryset(self, request):
+        qs = super(ItemAgendaAdmin, self).queryset(request)
+        return qs.filter(usuario=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.usuario = request.user
+        obj.save()
+
+admin.site.register(ItemAgenda, ItemAgendaAdmin)
+
