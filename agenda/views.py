4369e45c5cd339f5049303c65ad404832edbfc72
Osvaldo Santana <osantana@triveos.com>
2010-10-27 16:06:17 -0200

    Correcoes de codigo e melhorias

--- agenda/views.py
+++ agenda/views.py
@@ -2,6 +2,8 @@
 
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
 
 from models import ItemAgenda
 from forms import FormItemAgenda
@@ -22,12 +24,13 @@ def adiciona(request):
             item.save()
 
             # Mensagem de formulário cadastrado
-            return render_to_response("salvo.html", {})
+            return HttpResponseRedirect("/")
     else:
         # Exibe formulário em branco
         form = FormItemAgenda()
 
-    return render_to_response("adiciona.html", {'form': form})
+    return render_to_response("adiciona.html", {'form': form},
+            context_instance=RequestContext(request))
 
 @login_required
 def item(request, nr_item):
@@ -36,17 +39,19 @@ def item(request, nr_item):
         form = FormItemAgenda(request.POST, request.FILES, instance=item)
         if form.is_valid():
             form.save()
-            return render_to_response("salvo.html", {})
+            return HttpResponseRedirect("/")
     else:
         form = FormItemAgenda(instance=item)
-    return render_to_response("item.html", {'form': form})
+    return render_to_response("item.html", {'form': form},
+            context_instance=RequestContext(request))
 
 @login_required
 def remove(request, nr_item):
     item = get_object_or_404(ItemAgenda, usuario=request.user, id=nr_item)
     if request.method == "POST":
         item.delete()
-        return render_to_response("removido.html", {})
+        return HttpResponseRedirect("/")
     else:
-        return render_to_response("remove.html", {'item': item})
+        return render_to_response("remove.html", {'item': item},
+                context_instance=RequestContext(request))
 
